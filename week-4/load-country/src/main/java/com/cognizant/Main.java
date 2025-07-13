1222b332278857d6a5a9e4d4359c21a68707e66e
Rachana-Jeganathan <rachanajeganathan@gmail.com>
2025-07-13 15:05:07 +0530

    Add files via upload

---
+++ week-4/load-country/src/main/java/com/cognizant/Main.java
@@ -0,0 +1,21 @@
+package com.cognizant;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        displayCountry();
+    }
+
+    public static void displayCountry() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        Country country = context.getBean("country", Country.class);
+        LOGGER.debug("Country : {}", country.toString());
+    }
+}
